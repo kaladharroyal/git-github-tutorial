37a8ecd206453662f52fed17a9a359dd7464d570
Kaladharroyal <kaladharroyal@gmail.com>
2026-07-22 23:46:35 +0530

    locked main.py using uv lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from hello!")
+import time
+from rich.progress import track
 
-
-if __name__ == "__main__":
-    main()
+for i in track(range(20), description="For example:"):
+    time.sleep(0.05)
+    
